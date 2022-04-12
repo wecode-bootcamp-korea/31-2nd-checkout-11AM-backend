ee7bd4690c3e25ed2befc98fa0ca191bb29f27d8
DaeHyunKim <kimdaehyun10806@gmail.com>
2022-04-12 19:55:38 +0900

    [NEW] 모델링 전체 수정

---
+++ users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from utilities.timestamp import TimeStamp
+
+class User(TimeStamp):
+    kakao_id       = models.IntegerField()
+    nickname      = models.CharField(max_length=50, unique=True)
+    email         = models.CharField(max_length=100, null=True)
+    
+    class Meta:
+        db_table = 'users'
+        
+class Wishlist(models.Model):
+    user      = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    residence = models.ForeignKey('residences.Residence', on_delete=models.CASCADE)
+    room      = models.ForeignKey('residences.Room', on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'wishlists'
+        constraints = [models.UniqueConstraint(
+            fields = ['user', 'residence'],
+            name = 'wishlists_user_residence_unq'
+            )
+        ]
