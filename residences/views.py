9805dc3731f3bc4243d9ec7fd24c59c0eae409ab
재홍 <jaehong@jaehongs-MacBook-Air.local>
2022-04-19 15:36:54 +0900

    Code Review

--- residences/views.py
+++ residences/views.py
@@ -1,34 +1,24 @@
-from django.http import JsonResponse
-from django.views import View
-from .models import *
-
 from django.http            import JsonResponse
 from django.views           import View
 
-from residences.models         import Residence
+from .models import RegionEnum, Residence
+
 
 class ResidenceListView(View):
     def get(self, request):
         limit     = int(request.GET.get('limit', 10))
         offset    = int(request.GET.get('offset', 0))
         sort      = request.GET.get('sort', 'id')
-        region    = request.GET.getlist('region', ['제주시', '서귀포시', '동부', '서부'])
+        region    = request.GET.getlist('region', RegionEnum.get_regions())
 
         filter_options = {
             'category'  : 'category__title',
             'search'    : 'name__icontains',
             'people'    : 'rooms__max_person__gte',
             'min-price' : 'rooms__price__gte',
-            'max-price' : 'rooms__price__lte'
+            'max-price' : 'rooms__price__lte',
+            'region'    : 'region__title__in' 
         }
-
-        filter_set = {
-            filter_options.get(key): value\
-                for (key, value) in request.GET.items()\
-                     if filter_options.get(key)
-        }
-        filter_set['region__title__in'] = region
-
         sort_type = {
             'id'        : 'id',
             'new'       : '-created_at',
@@ -36,9 +26,8 @@ class ResidenceListView(View):
             'low-price' : 'room__price'
         }
 
-        residences = Residence.objects.filter(**filter_set)\
-                        .prefetch_related('rooms')\
-                        .order_by(sort_type[sort])[offset:offset+limit]
+        filter_set = {filter_options.get(key): value for (key, value) in request.GET.items() if filter_options.get(key)}
+        residences = Residence.objects.filter(**filter_set).select_related('region', 'category').prefetch_related('rooms', 'residence_images').order_by(sort_type[sort])[offset:offset+limit]
 
         residences_list = [{
             'id'       : residence.id,
